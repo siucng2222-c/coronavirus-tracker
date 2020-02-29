4a87d1ae6ee1ca80e040946754c058907d7e1550
xlogger <xloggerster@gmail.com>
2020-02-29 17:54:29 +0800

    define LocationStats model and cachedStats list

---
+++ src/main/java/com/scallionlead/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,25 @@
+package com.scallionlead.coronavirustracker.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * LocationStats
+ */
+// https://www.baeldung.com/intro-to-project-lombok
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class LocationStats {
+
+    public static final String PROVINCE_STATE_FIELD = "Province/State";
+    public static final String COUNTRY_REGION_FIELD = "Country/Region";
+
+    private String provinceState;
+    private String countryRegion;
+    private int latestTotalCases;
+
+}
--- src/main/java/com/scallionlead/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/scallionlead/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,16 +1,17 @@
 package com.scallionlead.coronavirustracker.services;
 
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
+
+import com.scallionlead.coronavirustracker.models.LocationStats;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
@@ -24,6 +25,8 @@ import org.springframework.stereotype.Service;
 public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/siucng2222-c/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+
+    private List<LocationStats> cachedStats = new ArrayList<>();
 
     // Execute this method after Spring app started and service bean
     // constructed
@@ -39,12 +42,26 @@ public class CoronaVirusDataService {
         String responseStr = response.body();
         // System.out.println(responseStr);
         StringReader csvStringReader = new StringReader(responseStr);
+
+        List<LocationStats> newStats = new ArrayList<>();
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvStringReader);
         for (CSVRecord record : records) {
-            String provinceState = record.get("Province/State");
-            String countryRegion = record.get("Country/Region");
-            String lat = record.get("Lat");
-            System.out.printf(Locale.getDefault(), "[%s,%s,%s] \n", provinceState, countryRegion, lat);
+            // String provinceState = record.get("Province/State");
+            // String countryRegion = record.get("Country/Region");
+            // String lat = record.get("Lat");
+            // System.out.printf(Locale.getDefault(), "[%s,%s,%s] \n", provinceState,
+            // countryRegion, lat);
+
+            LocationStats stats = new LocationStats();
+            stats.setProvinceState(record.get(LocationStats.PROVINCE_STATE_FIELD));
+            stats.setCountryRegion(record.get(LocationStats.COUNTRY_REGION_FIELD));
+
+            // Get the latest stat from last column
+            stats.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+            System.out.println(stats);
+            newStats.add(stats);
         }
+
+        this.cachedStats = newStats;
     }
 }
